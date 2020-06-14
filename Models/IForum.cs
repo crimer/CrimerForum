5814fff859fbffa93c6c2fc6440636e83033ef67
crimer <99nik.pro@gmail.com>
2020-06-14 21:12:49 +1000

    add services layer

---
+++ Models/IForum.cs
@@ -0,0 +1,22 @@
+﻿using CrimerForum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrimerForum.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateTitle(int forumId, string newTitle);
+        Task UpdateImage(int forumId, string newUrlImage);
+        Task UpdateDescription(int forumId, string newDescription);
+
+    }
+}
---
+++ Services/ForumService.cs
@@ -0,0 +1,60 @@
+﻿using CrimerForum.Data;
+using CrimerForum.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ForumService : IForum
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ForumService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            return _context.Forums
+                .Include(f => f.Posts);
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Forum GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateImage(int forumId, string newUrlImage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
